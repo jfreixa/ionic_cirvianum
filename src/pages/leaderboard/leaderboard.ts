2390198ad780724fc5e16987961d378dc6ee0c8d
Sergio Novo <sergionovo1997@gmail.com>
2018-05-04 18:57:34 +0200

    Leaderboard get usuaris

--- src/pages/leaderboard/leaderboard.ts
+++ src/pages/leaderboard/leaderboard.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'page-leaderboard',
   templateUrl: 'leaderboard.html'
 })
 export class LeaderboardPage {
+  public topPlayers;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public db: AngularFirestore) {
+    db.collection('usuaris', ref => ref.limit(10)).valueChanges().subscribe(val => {
+      this.topPlayers = val.sort(this.ordenarPasses);
+    });
+  }
 
+  private ordenarPasses(a, b) {
+    if (a.passes < b.passes)
+      return 1;
+    if (a.passes > b.passes)
+      return -1;
+    return 0;
   }
 
 }
