cb485b98fb38bce8c546b82350ca62e5364fed4d
Sergio Novo <sergionovo1997@gmail.com>
2018-05-04 18:30:13 +0200

    Afegit login + agafar dades usuari

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFirestore } from 'angularfire2/firestore';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public topPlayers: any;
-  
-  constructor(public navCtrl: NavController, public db: AngularFirestore) {
+  public topPlayers: Array<object>;
+  public usuari: object;
 
-    db.collection('usuaris').valueChanges().subscribe(val => {
+  constructor(public navCtrl: NavController, public db: AngularFirestore, private storage: Storage) {
+    this.storage.get('uid').then(key => {
+      db.doc(`usuaris/${key}`).valueChanges().subscribe(val => {
+        if(val) this.usuari = val;
+      });
+    });
+
+
+    db.collection('usuaris', ref => ref.limit(3)).valueChanges().subscribe(val => {
       this.topPlayers = val.sort(this.ordenarPasses);
-      console.log(val);
     });
   }
   private ordenarPasses(a, b) {
