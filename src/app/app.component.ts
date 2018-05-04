bc113d18bcf25a2c3308bab180bf9729a2d92a54
Sergio Novo <sergionovo1997@gmail.com>
2018-05-04 16:58:24 +0200

    Added firebase realtime users sortets by passes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,38 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { AngularFirestore } from 'angularfire2/firestore';
+import * as firebase from 'firebase';
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+export class MyApp {
+  rootPage: any = HomePage;
+  public topPlayers: any;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, db: AngularFirestore) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    const settings: firebase.firestore.Settings = { timestampsInSnapshots: true };
+    db.app.firestore().settings(settings);
+
+    db.collection('usuaris').valueChanges().subscribe(val => {
+      this.topPlayers = val.sort(this.ordenarPasses);
+      console.log(val);
+    });
+  }
+
+  private ordenarPasses(a, b) {
+  if (a.passes < b.passes)
+    return 1;
+  if (a.passes > b.passes)
+    return -1;
+  return 0;
   }
 }
-
